74fb53024af65a57b732066af6d06d30b4fa9fa9
peterchang0414 <urbana0414@naver.com>
2022-03-29 10:39:22 +0000

    Finalize modern.py

--- modern.py
+++ modern.py
@@ -1,4 +1,4 @@
-# Backpropagation Applied to MNIST (1989 Version)
+# Backpropagation Applied to MNIST (Modern Version)
 # Based on Lecun 1989: http://yann.lecun.com/exdb/publis/pdf/lecun-89e.pdf
 # Adapted to JAX from https://github.com/karpathy/lecun1989-repro/blob/master/prepro.py
 # Author: Peter G. Chang (@peterchang0414)
@@ -35,17 +35,22 @@ def get_datasets(key, n_tr, n_te):
     return train_test
 
 class Net(nn.Module):
+    training: bool
     bias_init: Callable = nn.initializers.zeros
     # sqrt(6) = 2.449... used by he_uniform() approximates Karpathy's 2.4
     kernel_init: Callable = nn.initializers.he_uniform()
 
     @nn.compact
     def __call__(self, x):
+        if self.training:
+            augment_rng = self.make_rng('aug')
+            shift_x, shift_y = jax.random.randint(augment_rng, (2,), -1, 2)
+            x = jnp.roll(x, (shift_x, shift_y), (1, 2))
         x = jnp.pad(x, [(0,0),(2,2),(2,2),(0,0)], constant_values=-1.0)
         x = nn.Conv(features=12, kernel_size=(5,5), strides=2, padding='VALID',
                     use_bias=False, kernel_init=self.kernel_init)(x)
         bias1 = self.param('bias1', self.bias_init, (8, 8, 12))
-        x = tanh(x + bias1)
+        x = nn.relu(x + bias1)
         x = jnp.pad(x, [(0,0),(2,2),(2,2),(0,0)], constant_values=-1.0)
         x1, x2, x3 = (x[..., 0:8], x[..., 4:12], 
                       jnp.concatenate((x[..., 0:4], x[..., 8:12]), axis=-1))
@@ -57,28 +62,36 @@ class Net(nn.Module):
                          use_bias=False, kernel_init=self.kernel_init)(x3)
         x = jnp.concatenate((slice1, slice2, slice3), axis=-1)
         bias2 = self.param('bias2', self.bias_init, (4, 4, 12))
-        x = tanh(x + bias2)
+        x = nn.relu(x + bias2)
+        x = nn.Dropout(0.25, deterministic=not self.training)(x)
         x = x.reshape((x.shape[0], -1))
         x = nn.Dense(features=30, use_bias=False)(x)
         bias3 = self.param('bias3', self.bias_init, (30,))
-        x = tanh(x + bias3)
+        x = nn.relu(x + bias3)
         x = nn.Dense(features=10, use_bias=False)(x)
-        bias4 = self.param('bias4', nn.initializers.constant(-1.0), (10,))
-        x = tanh(x + bias4)
+        bias4 = self.param('bias4', self.bias_init, (10,))
+        x = x + bias4
         return x
 
-def create_train_state(key, lr, X):
-    model = Net()
-    params = model.init(key, X)['params']
-    sgd_opt = optax.sgd(lr)
-    return train_state.TrainState.create(apply_fn=model.apply, 
-                                         params=params, tx=sgd_opt)
+def learning_rate_fn(initial_rate, epochs, steps_per_epoch):
+    return optax.linear_schedule(init_value=initial_rate, end_value=initial_rate/3,
+                                 transition_steps=epochs*steps_per_epoch)
+
+def create_train_state(key, X, lr_fn):
+    model = Net(training=True)
+    key1, key2, key3 = jax.random.split(key, 3)
+    params = model.init({'params': key1, 'aug': key2, 'dropout': key3}, X)['params']
+    opt = optax.adamw(lr_fn)
+    return train_state.TrainState.create(apply_fn=model.apply, params=params, tx=opt)
 
 @jax.jit
-def train_step(state, X, Y):
+def train_step(state, X, Y, rng=jax.random.PRNGKey(0)):
+    aug_rng, dropout_rng = jax.random.split(jax.random.fold_in(rng, state.step))
     def loss_fn(params):
-        Yhat = Net().apply({'params': params}, X)
-        loss = jnp.mean((Yhat - Y)**2)
+        Yhat = Net(training=True).apply({'params': params}, X, 
+                                        rngs={'aug': aug_rng,
+                                              'dropout': dropout_rng})
+        loss = jnp.mean(optax.softmax_cross_entropy(logits=Yhat, labels=Y))
         err = jnp.mean(jnp.argmax(Y, -1) != jnp.argmax(Yhat, -1)).astype(float)
         return loss, err
     (_, Yhats), grads = jax.value_and_grad(loss_fn, has_aux=True)(state.params)
@@ -94,9 +107,10 @@ def train_one_epoch(state, X, Y):
 def train(key, data, epochs, lr):
     Xtr, Ytr = data['train']
     Xte, Yte = data['test']
-    train_state = create_train_state(key, lr, Xtr)
+    lr_fn = learning_rate_fn(lr, epochs, Xtr.shape[0])
+    train_state = create_train_state(key, Xtr, lr_fn)
     for epoch in range(epochs):
-        print(f"epoch {epoch+1}")
+        print(f"epoch {epoch+1} with learning rate {lr_fn(train_state.step):.6f}")
         train_state = train_one_epoch(train_state, Xtr, Ytr)
         for split in ['train', 'test']:
             eval_split(data, split, train_state.params)
@@ -104,8 +118,8 @@ def train(key, data, epochs, lr):
 
 @jax.jit
 def eval_step(params, X, Y):
-    Yhat = Net().apply({'params': params}, X)
-    loss = jnp.mean((Yhat - Y)**2)
+    Yhat = Net(training=False).apply({'params': params}, X)
+    loss = jnp.mean(optax.softmax_cross_entropy(logits=Yhat, labels=Y))
     err = jnp.mean(jnp.argmax(Y, -1) != jnp.argmax(Yhat, -1)).astype(float)
     return loss, err
 
@@ -118,13 +132,13 @@ def eval_split(data, split, params):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Train a 1989 LeCun ConvNet on digits")
-    parser.add_argument('--learning-rate', '-l', type=float, default=0.03, help="SGD learning rate")
-    parser.add_argument('--output-dir'   , '-o', type=str,   default='out/1989', help="output directory for training logs")
+    parser.add_argument('--learning-rate', '-l', type=float, default=3e-4, help="Learning rate")
+    parser.add_argument('--output-dir'   , '-o', type=str,   default='out/modern', help="output directory for training logs")
     args = parser.parse_args()
     print(vars(args))
     key1, key2 = jax.random.split(jax.random.PRNGKey(42))
-    data = get_datasets(key1, 10, 10)
-    state = train(key2, data, 2, args.learning_rate)
+    data = get_datasets(key1, 7291, 2007)
+    state = train(key2, data, 23, args.learning_rate)
     bytes_output = serialization.to_bytes(state.params)
     os.makedirs(os.path.dirname(args.output_dir), exist_ok=True)
     with open(args.output_dir, 'wb') as f:
